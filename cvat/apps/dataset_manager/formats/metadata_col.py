1789687e0ec48072e569efac3f95ef8e7f769ebb
ZijinChen <zijin.chen@wspdigital.com>
2022-06-10 12:59:45 +1000

    added column labels format

--- cvat/apps/dataset_manager/formats/metadata_col.py
+++ cvat/apps/dataset_manager/formats/metadata_col.py
@@ -1,4 +1,4 @@
-### Custom Export Format for WSP Ecology Team - allow one label
+### Custom Export Format for WSP Ecology Team - allow multiple labels
 
 from asyncore import write
 from tempfile import TemporaryDirectory, TemporaryFile
@@ -36,37 +36,69 @@ def get_img_metadata(img_name):
             return None, None
 
 def write_to_csv_task(f, task_data):
-    # iterate over all frames
+    # add labels columns
+    title = False
+    tags_num = []
+    for frame_annotation in task_data.group_by_frame():
+        tag_num = 0
+        for tags in frame_annotation.tags:
+            tag_num += 1
+        tags_num.append(tag_num)
+    max_tag = max(tags_num)
+    label_title = ''
+    for i in range(max_tag):
+        label_title = label_title+',Label'+str(i+1)
+    f.write('Project name,Camera name,Image name,Date,Time'+label_title+',\n')
+
+    # add date, time, labels info for each image
     for frame_annotation in task_data.group_by_frame():
         image_path = frame_annotation.name
         project = image_path.split('/')[0]
         camera = image_path.split('/')[1]
         image_name = image_path.split('/')[-1]
         capture_date, capture_time = get_img_metadata(image_path)
+        start_str = project+','+camera+','+image_name+','+str(capture_date)+','+str(capture_time)
+        label_str = ''
         for tags in frame_annotation.tags:
             label = tags.label
-            f.write(project+','+camera+','+image_name+','+str(capture_date)+','+str(capture_time)+ ','+label+'\n')
+            label_str = label_str +','+ label
+        f.write(start_str+ label_str+'\n')
     f.close()
 
 def write_to_csv_project(f, project_data):
-    # iterate over all frames
+    # add labels columns
+    title = False
+    tags_num = []
+    for frame_annotation in project_data.group_by_frame():
+        tag_num = 0
+        for tags in frame_annotation.tags:
+            tag_num += 1
+        tags_num.append(tag_num)
+    max_tag = max(tags_num)
+    label_title = ''
+    for i in range(max_tag):
+        label_title = label_title+',Label'+str(i+1)
+    f.write('Project name,Camera name,Image name,Date,Time'+label_title+',\n')
+
+    # add date, time, labels info for each image
     for frame_annotation in project_data.group_by_frame():
         image_path = frame_annotation.name
         project = image_path.split('/')[0]
         camera = image_path.split('/')[1]
         image_name = image_path.split('/')[-1]
         capture_date, capture_time = get_img_metadata(image_path)
+        start_str = project+','+camera+','+image_name+','+str(capture_date)+','+str(capture_time)
+        label_str = ''
         for tags in frame_annotation.tags:
             label = tags.label
-            f.write(project+','+camera+','+image_name+','+str(capture_date)+','+str(capture_time)+ ','+label+'\n')
-
+            label_str = label_str +','+ label
+        f.write(start_str+ label_str+'\n')
     f.close()
 
 
 def _export_task(dst_file, task_data, save_images=False):
     with TemporaryDirectory() as temp_dir:
-        with open(osp.join(temp_dir, 'task_annotations.csv'), 'w') as f:
-            f.write('Project name,Camera name,Image name,Date,Time,Label,\n')
+        with open(osp.join(temp_dir, 'task_annotations_labels.csv'), 'w') as f:
             write_to_csv_task(f, task_data)
 
         make_zip_archive(temp_dir, dst_file)
@@ -74,14 +106,13 @@ def _export_task(dst_file, task_data, save_images=False):
 
 def _export_project(dst_file: str, project_data: ProjectData, save_images: bool=False):
      with TemporaryDirectory() as temp_dir:
-        with open(osp.join(temp_dir, 'project_annotations.csv'), 'w') as f:
-            f.write('Project name,Camera name,Image name,Date,Time,Label,\n')
+        with open(osp.join(temp_dir, 'project_annotations_labels.csv'), 'w') as f:
             write_to_csv_task(f, project_data)
 
         make_zip_archive(temp_dir, dst_file)
 
 
-@exporter(name='Animal Crossing Row Labels', ext='ZIP', version='1.0')
+@exporter(name='Animal Crossing Column Labels', ext='ZIP', version='1.0')
 def _export_images(dst_file, instance_data, save_images=False):
     if isinstance(instance_data, TaskData):
         _export_task(dst_file, instance_data,save_images=save_images)
